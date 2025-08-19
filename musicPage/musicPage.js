dd6bf886a84eaeeadfa6a6174acc3bf1d28b61f5
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2025-08-19 21:34:00 +0800

    Added a youtube link.

--- musicPage/musicPage.js
+++ musicPage/musicPage.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const spotifyLink = "https://open.spotify.com/album/0IcJ6YotAaglvsmCpBg6qh?si=8ECwc7IBQ-Oy5PzEKNBPnQ";
+  const youtubeLink = "https://www.youtube.com/watch?v=exekHnjoyoM&list=PLWw3ZlyPoHmEEkE2xXz9ZhMaNR-wu_OFy";
+
   const spotifyLogo = document.querySelector(".spotifyLogo");
+  const youtubeLogo = document.querySelector(".youtubeLogo");
+
   if (spotifyLogo) {
     spotifyLogo.addEventListener("click", () => {
-      window.open("https://open.spotify.com/album/0IcJ6YotAaglvsmCpBg6qh?si=8ECwc7IBQ-Oy5PzEKNBPnQ", "_blank");
+      window.open(spotifyLink, "_blank");
+    });
+  }
+
+  if (youtubeLogo) {
+    youtubeLogo.addEventListener("click", () => {
+      window.open(youtubeLink, "_blank");
     });
   }
 });
