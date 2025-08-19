894e9ad54fc78a6ed3eccf728e97ee4ad6964ae8
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2025-08-19 20:55:39 +0800

    Fixed the footer amongst other things.

--- musicPage/musicPage.js
+++ musicPage/musicPage.js
@@ -6,10 +6,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-
-fetch("/navbar/navbar.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("navbar-placeholder").innerHTML = data;
-    });
